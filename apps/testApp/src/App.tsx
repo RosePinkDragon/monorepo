5253c416b210db8eeec85b116ba28c861cb9b18b
RosePinkDragon <mohebdabilkar@gmail.com>
2024-04-14 16:15:34 +0530

    add submit btn to form in app

--- apps/testApp/src/App.tsx
+++ apps/testApp/src/App.tsx
@@ -2,12 +2,25 @@ import FormGenerator from "@mmd/core";
 import formData from "./data/testForm";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers";
+import { useFormikContext } from "formik";
+import { Button } from "@mui/material";
+
+const FormSubmitBtn = () => {
+  const { submitForm } = useFormikContext();
+
+  return (
+    <Button onClick={submitForm} variant="contained" color="primary">
+      Hello Submimt
+    </Button>
+  );
+};
 
 function App() {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <FormGenerator
         isViewOnly={false}
+        FormEndComponent={<FormSubmitBtn />}
         submitHandler={(e) => console.log(e)}
         formData={formData}
       />
